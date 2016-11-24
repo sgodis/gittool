ad582d367bb72f1a12118f4f768d5b536ff0c59f
huangxiaorong <357770466@qq.com>
2016-11-24 21:41:27 +0800

    modify conf

--- conf.sh
+++ conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+# 项目根目录
 ROOT=/Users/huangxiaorong/vagrant/centos_data/mysoft/cgzs_gys/
+# 项目根目录底下包含的所有需要进行操作的目录
 FLODERS=(business caigouzhushou config framework passport vendor)
-# ROOT=/Users/huangxiaorong/vagrant/centos_data/gittool/
-# FLODERS=(gitmerge)
 
